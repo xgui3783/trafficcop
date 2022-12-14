fcc1d21785262ff78f0f93387dbdba986fa69d7b
Xiao Gui <xgui3783@gmail.com>
2022-12-14 14:21:19 +0100

    listening to all paths

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const REDIRECT_URL = process.env.REDIRECT_URL
 if (!REDIRECT_URL) throw new Error(`REDIRECT_URL not defined`)
 
 const express = require('express')
+const { URL } = require("url")
 
 const app = express()
 
 console.log(`redirecting / to ${REDIRECT_URL}`)
 
-app.get('/', (req, res) => {
-  console.log(`redirecting`)
-  res.redirect(REDIRECT_URL)
+app.get('*', (req, res) => {
+  const { path, query } = req
+  const url = new URL(REDIRECT_URL.replace(/\/+$/, '') + path)
+  for (const key in query) {
+    url.searchParams.set(key, query[key])
+  }
+  res.redirect(url)
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
